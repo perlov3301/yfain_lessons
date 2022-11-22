c988e1c7dd19a28171f27b79bb7f6c80a6e65b60
perlov3301 <perlov3301@gmail.com>
2022-11-22 21:11:40 +0200

    origin

---
+++ src/lesson01/Lesson1.java
@@ -0,0 +1,10 @@
+package lesson01;
+
+public class Lesson1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World from lesson01");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author perlo
+ *
+ */
+module YFain1 {
+}
