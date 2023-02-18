78c683b07c337d6563a43b7cdf237f579be49104
Shlomo Alexander Perlov <perlov3301@gmail.com>
2023-02-18 12:34:09 -0500

    lesson06 bikes button order

--- src/lesson06/CgridbagListener.java
+++ src/lesson06/CgridbagListener.java
@@ -10,6 +10,7 @@ public class CgridbagListener {
    JTextField fieldmodel;
    JTextField fieldquantity;
    JTextField fieldInfo;
+   JLabel label;
    JLabel labelP;
    JLabel labelM;
    JLabel labelQ;
@@ -32,57 +33,53 @@ public class CgridbagListener {
 	   CgridbagListenerEngine engine = new CgridbagListenerEngine(this);
 	   GridBagConstraints c = new GridBagConstraints();
 	   c.fill = GridBagConstraints.HORIZONTAL;
-	   c.weightx = 0.5;
+	  // c.weightx = 1;
 // Insets(int top, int left, int bottom, int right)
-	   labelP = new JLabel("popular:");
-	   c.gridx = 0;
+	  labelP = new JLabel("place the Order");
+	   c.gridx = 2;
 	   c.gridy = 0;
+	   c.gridwidth=2;
 	   c.insets = ins;
 	   pane.add(labelP, c);
-	   
-	   button = new JButton(" model1");
-	   c.gridx = 1;
+	  
+	   button = new JButton(" Order ");
+	   c.gridx = 4;
 	   c.gridy = 0;
 	   c.insets = ins;
 	   c.gridwidth = 2;
+	   c.ipady=10;
 	   button.addActionListener(engine);
 	   pane.add(button, c);
+	   c.ipady=0;
 	   
-	   button = new JButton(" model2 ");
-	   c.gridx = 3;
-	   c.gridy = 0;
-	   c.insets = ins;
+	   button = new JButton("reset Order");
+	   c.gridx= 6;
+	   c.gridy= 0;
 	   c.gridwidth = 2;
-	   button.addActionListener(engine);
-	   pane.add(button, c);
-	   
-	   button = new JButton(" model3 ");
-	   c.gridx = 5;
-	   c.gridy = 0;
-	   c.insets = ins;
-	   c.gridwidth = 2;
+	   c.insets= ins;
 	   button.addActionListener(engine);
 	   pane.add(button, c);
 	   
 	   labelM = new JLabel("awaiting");
-	   //labelM.setForeground(Color.yellow);
 	   c.gridx = 0;
 	   c.gridy = 1;
-//	   c.gridwidth=2;
+	   c.gridwidth=2;
+	   c.insets = ins;
        pane.add(labelM,c);	
        
-	   fieldmodel = new JTextField("");
-	   c.gridx =1;
+	   fieldmodel = new JTextField("                       ");
+	   c.gridx =2;
 	   c.gridy = 1;
-	   c.gridwidth = 3;
+	   c.gridwidth = 2;
+	   c.insets = ins;
 	   pane.add(fieldmodel, c);
 	   
 	   button = new JButton("add model");
-	   c.ipady = 20;
 	   c.gridx = 4;
 	   c.gridy = 1;
 	   c.gridwidth = 2;
 	   c.insets = ins;
+	   c.ipady = 10;
 	   button.addActionListener(engine);
 	   pane.add(button, c);
 	   c.ipady = 0;
@@ -91,26 +88,30 @@ public class CgridbagListener {
 	   c.gridx = 6;
 	   c.gridy = 1;
 	   c.insets = ins;
+	   c.gridwidth=2;
 	   button.addActionListener(engine);
 	   pane.add(button, c);
 	   
 	   labelQ = new JLabel("awaiting");
 	   c.gridx = 0;
 	   c.gridy = 2;
+	   c.insets= ins;
+	   c.gridwidth=2;
        pane.add(labelQ,c);	
        
-	   fieldquantity = new JTextField("");
-	   c.gridx = 1;
+	   fieldquantity = new JTextField("                       ");
+	   c.gridx = 2;
 	   c.gridy = 2;
-	   c.gridwidth = 3;
+	   c.gridwidth = 2;
+	   c.insets= ins;
 	   pane.add(fieldquantity, c);
 	   
 	   button = new JButton("add quantity");
-	   c.ipady = 20;
 	   c.gridx = 4;
 	   c.gridy = 2;
 	   c.gridwidth = 2;
 	   c.insets = ins;
+	   c.ipady = 10;
 	   button.addActionListener(engine);
 	   pane.add(button, c);
 	   c.ipady = 0;
@@ -119,29 +120,24 @@ public class CgridbagListener {
 	   c.gridx = 6;
 	   c.gridy = 2;
 	   c.insets = ins;
+	   c.gridwidth=2;
 	   button.addActionListener(engine);
 	   pane.add(button, c);
 	   
-	   labelInfo= new JLabel("Notice         ");
-	   //labelInfo.setForeground(Color.yellow);
+	   labelInfo= new JLabel("Notice");
 	   c.gridx = 0;
 	   c.gridy = 3;
 	   c.gridwidth = 2;
 	   c.insets = ins;
        pane.add(labelInfo,c);	
        
-       fieldInfo = new JTextField("there is no Notice");
+       fieldInfo = new JTextField("there is no Notice       ");
 	   c.gridx = 2;
 	   c.gridy = 3;
-	   c.gridwidth = 5;
+	   c.gridwidth = 6;
 	   c.insets = ins;
 	   pane.add(fieldInfo, c);
 	   
-//	   keys = new CgridKeysPanelL(engine);
-//	   c.gridx = 0;
-//	   c.gridy = 2;
-//	   c.gridwidth = 3;
-//	   pane.add(keys, c);
    }
    String getDisplayValueM() {
 	   String fieldValue = fieldmodel.getText();
--- src/lesson06/CgridbagListenerEngine.java
+++ src/lesson06/CgridbagListenerEngine.java
@@ -17,11 +17,13 @@ public class CgridbagListenerEngine implements ActionListener {
 		parent.labelM.setForeground(Color.black);
 		parent.labelM.setText("awaiting");
 		parent.fieldInfo.setText("there is no Notice");
+		parent.fieldmodel.setText("");
 	}
 	private void setQok() {
 		parent.labelQ.setForeground(Color.black);
 		parent.labelQ.setText("awaiting");
 		parent.fieldInfo.setText("there is no Notice");
+		parent.fieldquantity.setText("");
 	}
 	public void actionPerformed(ActionEvent e) {
 		// get button sign
@@ -29,39 +31,44 @@ public class CgridbagListenerEngine implements ActionListener {
 		String buttonL = clickButton.getText();
 		buttonL = buttonL.toLowerCase();
 		String buttonT = buttonL.replaceAll("\\s", "");
-		if(buttonT.equals("model1") 
-		 | buttonT.equals("model2")
-		 | buttonT.equals("model3")) {
-			parent.setDislayValueM(buttonT);
-			signM =1;
+		if(buttonT.equals("order")) {
+			if(signM==1 && signQ==1) {
+				
+			}else {
+				
+			}
 		} else 
+			if (buttonT.equals("resetorder")) {
+				setMok();
+				setQok();
+				signM= -1;
+				signQ= -1;
+			}
+			else
 			if(buttonT.equals("resetmodel")) {
-			signM = 0;
+			signM = -1;
 			parent.setDislayValueM("");
-//			parent.labelM.setForeground(Color.black);
-//			parent.labelM.setText("awaiting");
 			setMok();
 		} else
 			if (buttonT.equals("resetquantity")) {
 			parent.setDislayValueQ("");
-//			parent.labelQ.setForeground(Color.black);
-//			parent.labelQ.setText("awaiting");
 			setQok();
-			signQ = 0;
+			signQ = -1;
 		} else 
 		    if (buttonT.equals("addmodel")){
 			sM = parent.getDisplayValueM();
 			String sMm = sM.replaceAll("\\s", "");
-			if (sMm.equals("") | sMm.equals(null)) {
+			if (sMm.equals(null) | sMm.isEmpty()) {
 				parent.labelM.setForeground(Color.RED);
 				parent.labelM.setText("see Notice");
 				parent.fieldInfo.setText("fill the Field for Model,please");
+				signM= -1;
 			} else {
 				setMok();
 				parent.labelM.setForeground(Color.GREEN);
-				parent.labelM.setText("added");;
+				parent.labelM.setText("added");
+				signM= 1;
 			}
-			
 		} else
 			if (buttonT.equals("addquantity")) {
 				sQ = parent.getDisplayValueQ();
@@ -71,17 +78,20 @@ public class CgridbagListenerEngine implements ActionListener {
 						setQok();
 						parent.labelQ.setForeground(Color.GREEN);
 						parent.labelQ.setText("added");
+						signQ=1;
 					} else {
 						parent.labelQ.setForeground(Color.RED);
 						parent.labelQ.setText("see Notice");
 						parent.fieldInfo.setText("Quantity must be biger than 0");
+						signQ=0;
 					}
 				} catch (NumberFormatException ne) {
-					System.out.println("CgridbagListenerEngine: " + ne.getMessage());
-					ne.getMessage();
+					//System.out.println("CgridbagListenerEngine: " + ne.getMessage().toString());
+					//ne.printStackTrace();
 					parent.labelQ.setForeground(Color.RED);
 					parent.labelQ.setText("see Notice");
 					parent.fieldInfo.setText("Quantity must be a Number");
+					signQ=-1;
 				}
 			
 			}
